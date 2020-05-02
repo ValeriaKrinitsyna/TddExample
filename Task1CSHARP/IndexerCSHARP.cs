f28008d23708603d1ab3fd57e920629ff847d143
Valeria Krinitsyna <lerakrip@gmail.com>
2020-05-02 11:35:48 +0300

    Task1 C# added

--- Task1CSHARP/IndexerCSHARP.cs
+++ Task1CSHARP/IndexerCSHARP.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Task1CSHARP
 {
-    class IndexerCSHARP
+    public class IndexerCSHARP
     {
+        double[] array;
+        int start, end;
+        public IndexerCSHARP(double[] array, int start, int length)
+        {
+            if (start < 0 || length <= 0 || array.Length <= start + length)
+                throw new ArgumentException();
+            this.array = array;
+            this.start = start;
+            this.end = start + length - 1;
+        }
+        public int Length
+        {
+            get { return end - start + 1; }
+        }
+
+        public double this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Length)
+                    throw new IndexOutOfRangeException();
+                return this.array[index + start];
+            }
+            set
+            {
+                if (index < 0 || index >= this.Length)
+                    throw new IndexOutOfRangeException();
+                this.array[index + start] = value;
+            }
+        }
     }
 }
